a8d5a994a73819b8cd99a49a6d7bdc8de5aabc34
donkeypagoda <christopherallenstewart@gmail.com>
2017-11-04 17:45:36 -0700

    clean up

--- poly.js
+++ poly.js
@@ -4,16 +4,5 @@ const helperPolygon = function (numbSides, size, centArr) {
   for (let i = 1; i <= numbSides; i++) {
       shape.vertices.push(new THREE.Vector3(centArr[0] + size * Math.cos(i * 2 * Math.PI / numbSides), centArr[1] + size * Math.sin(i * 2 * Math.PI / numbSides), centArr[2]));
   }
-  console.log(shape.vertices);
   return shape;
-  }
-
-
-
-// cxt.beginPath();
-// cxt.moveTo (Xcenter +  size * Math.cos(0), Ycenter +  size *  Math.sin(0));
-
-
-// cxt.strokeStyle = "#000000";
-// cxt.lineWidth = 1;
-// cxt.stroke();
+}
